800d6d5e1e691c194ae9a399daa43e4306eb29ec
Pavithira Manoharan <pavithira.manoharan@gmail.com>
2018-12-08 21:38:56 +0100

    Working changes made. To be tested.

---
+++ src/AddBook.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+
+class AddBook extends Component {
+    state = {
+        query: '',
+        filteredBooks: []
+    }
+
+    performSearch = (query) => {
+        this.setState({query: query})
+        if(this.state.query.length && this.state.query.length > 2) {
+            BooksAPI.search(this.state.query).then((searchResult) => {
+                this.setState({filteredBooks: searchResult})
+            }).catch(() => {
+                this.setState({filteredBooks: []})
+            })
+        }
+    }
+
+    moveShelf = (shelf, book) => {
+        this.props.allBooks.push(book)
+        this.props.moveShelf(shelf, book)
+    }
+
+    render() {
+           
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link className="close-search" to="/">Close</Link>
+                    <div className="search-books-input-wrapper">
+                        {
+                        /*
+                        NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                        You can find these search terms here:
+                        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                        you don't find a specific author or title. Every search is limited by search terms.
+                        */
+                        }
+                        <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={(event) => this.performSearch(event.target.value)}/>
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <ol className="books-grid">
+                        {(this.state.query.length) ? 
+                            (this.state.filteredBooks.length) ?
+                                this.state.filteredBooks.map((book) => (
+                                    <li key={book.id}>
+                                        <div className="book" key={book.name}>
+                                            <div className="book-top">
+                                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                                <div className="book-shelf-changer">
+                                                    <select onClick={(event) => this.moveShelf(event.target.value, book)}>
+                                                        <option value="move" disabled>Move to...</option>
+                                                        <option value="currentlyReading">Currently Reading</option>
+                                                        <option value="wantToRead">Want to Read</option>
+                                                        <option value="read">Read</option>
+                                                        <option value="none">None</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div className="book-title">{book.title}</div>
+                                            <div className="book-authors">
+                                                {(book.authors).map((author) => 
+                                                <span key={author} className='book-author'>{author}</span>
+                                                )}
+                                            </div>
+                                        </div>
+                                    </li>
+                                ))
+                                : (
+                                    <div>Sorry no books were found that matched your search!</div>
+                                )
+                            : ( 
+                                <div> Type something to start a search!</div>
+                         )}
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default AddBook
---
+++ src/ListBooks.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class ListBooks extends Component {
+
+    shelves = [
+        {
+            id: 'currentlyReading',
+            title: 'Currently Reading'
+        },
+        {
+            id: 'wantToRead',
+            title: 'Want to Read'
+        },
+        {
+            id: 'read',
+            title: 'Read'
+        }]
+
+    render() {
+        const { allBooks, moveShelf } = this.props
+        return(
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    {this.shelves.map((shelf) => (
+                        <div className="bookshelf" key={shelf.id}>
+                            <h2 className="bookshelf-title">{shelf.title}</h2>
+                            <div className="bookshelf-books">
+                                <ol className="books-grid">
+                                    {allBooks.filter((book) => {
+                                        return book.shelf === shelf.id
+                                    })
+                                    .map((book) => {
+                                        return <li key={book.id}>
+                                            <div className="book">
+                                                <div className="book-top">
+                                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                                    <div className="book-shelf-changer">
+                                                        <select onChange={(event) => moveShelf(event.target.value, book)} value={book.shelf}>
+                                                            <option value="move" disabled>Move to...</option>
+                                                            <option value="currentlyReading">Currently Reading</option>
+                                                            <option value="wantToRead">Want to Read</option>
+                                                            <option value="read">Read</option>
+                                                            <option value="none">None</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                <div className="book-title">{book.title}</div>
+                                                <div className="book-authors">
+                                                    {(book.authors).map((author) => 
+                                                    <span className='book-author' key={author}>{author}</span>
+                                                    )}
+                                                </div>
+                                            </div>
+                                        </li>
+                                    })
+                                    }
+                                </ol>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className="open-search">
+                    <Link to="/search">Add a book</Link>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ListBooks
